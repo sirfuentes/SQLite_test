054e4e1f0375283b9a50c4223a58e424286a5549
SirFuentes <sirfuentes7@gmail.com>
2017-03-26 23:36:16 +0200

    Login and register

---
+++ app/src/main/java/com/example/carlos/gettingfamiliar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.carlos.gettingfamiliar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.carlos.gettingfamiliar.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogout;
+    private Session session;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        session = new Session(this);
+        if(!session.loggedin()){
+            logout();
+        }
+        btnLogout = (Button)findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout(){
+        session.setLoggedin(false);
+        finish();
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+}
